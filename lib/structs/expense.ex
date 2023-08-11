0837aa0b3fe6ccbdbafd07a2f4be52fb6a4dc7bc
Alikhan Ondas <alikhan_ondas@MacBook-Pro-AdBerikkul.local>
2023-08-11 11:26:41 +0600

    add with method

--- lib/structs/expense.ex
+++ lib/structs/expense.ex
@@ -42,4 +42,30 @@ defmodule Tutorials.Structs.Expense do
     [expense | sample()]
   end
 
+  def update_amount(title, amount) do
+    [item] = Enum.filter(sample(), fn %{title: expense_title} -> expense_title == title end)
+    new_item = %{item | amount: amount}
+
+    [new_item | sample() |> List.delete(item)]
+  end
+
+  # --- with ---
+
+  @users ["Coco", "Cece", "Louis", "Chiko"]
+
+  def authenticate(user) when user in @users, do: {:ok, "authorized"}
+  def authenticate(_), do: {:error, "unauthorized"}
+
+  def verify_password(user, _password) when user in @users, do: {:ok, "password verified"}
+  def verify_password(_user, _password), do: {:error, "wrong password"}
+
+  def login(user, password) do
+    with {:ok, _auth_msg} <- authenticate(user),
+         {:ok, _msg} <- verify_password(user, password) do
+      {:ok, "#{user} logged in successfully"}
+    else
+      {:error, msg} -> {:error, msg}
+      _ -> :unauthorized
+    end
+  end
 end
